c641267a6ef6cb2e807749dac44a68b8df38f5c4
m-ehab2 <muhamedehab1@gmail.com>
2025-05-03 11:15:36 +0300

    Refactor Layout component to improve main section styling

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -6,7 +6,7 @@ const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-grow">
+      <main className="flex-1 flex">
         <Outlet />
       </main>
       <Footer />
