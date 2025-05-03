94659d61dc166fa83eae69434f0ff56aeeb2cb42
m-ehab2 <muhamedehab1@gmail.com>
2025-05-03 11:16:09 +0300

    Add ProtectedRoute component for route protection based on authentication status

---
+++ src/HOCs/ProtectedRoute.tsx
@@ -0,0 +1,18 @@
+import { Navigate } from "react-router";
+import { useAuth } from "../context/AuthContext";
+
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+}
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
+  const { isAuthenticated } = useAuth();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/admin" replace />;
+  }
+
+  return <>{children}</>;
+};
+
+export default ProtectedRoute;
